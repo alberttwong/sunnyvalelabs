a56d80dbaa5f71783bba0bfe0705ed944e055958
Albert T. Wong <atwong@alumni.uci.edu>
2024-08-20 11:54:11 -0700

    Update custodian_cronjob.sh

--- custodian_cronjob.sh
+++ custodian_cronjob.sh
@@ -1,2 +1,2 @@
 cd /home/ec2-user/sunnyvalelabs/
-~/.local/bin/custodian run --output-dir=logs custodian.yml
+~/.local/bin/c7n-mailer --config mailer.yml --update-lambda && ~/.local/bin/custodian run --output-dir=logs --metrics aws custodian.yml
